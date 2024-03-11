a6eb6482c10eaf0ef6f376321664527150a2462e
softman387 <manishkumarpatna2020@gmail.com>
2024-03-11 15:50:54 +0530

    Added User Api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
-import { errorHandler } from '../middleware/error.js';
+// import { errorHandler } from '../middleware/error.js';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
 export const test = (req, res) => {
@@ -12,6 +13,10 @@ export const test = (req, res) => {
 
 export const createUser = async(req, res) => {
     try {
+      //Checking User Exists or Not=============
+       const CheckUser = await User.findOne({ email:req.body.email });
+       CheckUser && res.status(400).json("Email Already Exists...");
+
         //generate new password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -38,49 +43,121 @@ export const createUser = async(req, res) => {
   };
 
 
-// update user
+  export const loginUser=async(req,res)=>{
+    try {
+      //find user
+      const user = await User.findOne({ email:req.body.email });
+      !user && res.status(400).json("Wrong email or password");
+  
+      //validate password=========================
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      !validPassword && res.status(400).json("Wrong Email or password");
 
-// export const updateUser = async (req, res, next) => {
-//   if (req.user.id !== req.params.id) {
-//     return next(errorHandler(401, 'You can update only your account!'));
-//   }
-//   try {
-//     if (req.body.password) {
-//       req.body.password = bcryptjs.hashSync(req.body.password, 10);
-//     }
+      //Generating Json Web Token==================
+      const token = jwt.sign({ id:user._id }, process.env.JWT_TOKEN_KEY);
+      const expiryDate = new Date(Date.now() + 3600000*120); // 1 hour*24hours*5days*
+       //send response
+      res
+        .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+        .status(200)
+        .json({msg:"Loged In Successful",user});
 
-//     const updatedUser = await User.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: {
-//           username: req.body.username,
-//           email: req.body.email,
-//           password: req.body.password,
-//           profilePicture: req.body.profilePicture,
-//         },
-//       },
-//       { new: true }
-//     );
-//     const { password, ...rest } = updatedUser._doc;
-//     res.status(200).json(rest);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+    } catch (err) {
+      res.status(500).json(err);
+    }
+
+  }
+
+//update user
+
+export const updateUser = async (req, res) => {
+  try {
+    if(req.params.id){
+      const user = await User.findByIdAndUpdate({_id:req.params.id},{$set:req.body},{new:true});
+      res.status(200).json({msg:"User Updated Successful",user});
+  }
+    // if (req.body.password) {
+    //   req.body.password = bcrypt.hashSync(req.body.password, 10);
+    // }
+
+    // const updatedUser = await User.findByIdAndUpdate(
+    //   req.params.id,
+    //   {
+    //     $set: {
+    //       firstname: req.body.firstname,
+    //       lastname: req.body.lastname,
+    //       mobile: req.body.mobile,
+    //       profile: req.body.profile,
+    //       role: req.body.role,
+    //       email: req.body.email,
+    //       password: hashedPassword,
+    //       profilePicture: req.body.profilePicture,
+    //     },
+    //   },
+    //   { new: true }
+    // );
+   // const { password, ...rest } = updatedUser._doc;
+
+   
+  } catch (error) {
+    res.status(500).json(err);
+  }
+};
+
+export const forgotPassword=async(req,res)=>{
+  try {
+    if(req.params.id){
+       if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+      const user = await User.findByIdAndUpdate({_id:req.params.id},{$set:{password:req.body.password}},{new:true});
+      res.status(200).json({msg:"Forgot Password Updated Successful",user});
+  }
+  } catch (error) {
+    res.status(500).json(err);
+  }
+
+}
 
 
-// delete user
+
+//Find All Users================================
+export const getAllUsers=async(req,res)=>{
+  try {
+    const findAllUsers = await User.find({});
+    !findAllUsers && res.status(400).json("Not Found!...");
+    res.status(200).json({msg:"User Found Successful",findAllUsers});
+  } catch (error) {
+    res.status(500).json(err);
+  }
+}
 
 
-// export const deleteUser = async (req, res, next) => {
-//   if (req.user.id !== req.params.id) {
-//     return next(errorHandler(401, 'You can delete only your account!'));
-//   }
-//   try {
-//     await User.findByIdAndDelete(req.params.id);
-//     res.status(200).json('User has been deleted...');
-//   } catch (error) {
-//     next(error);
-//   }
+//Find User By Id ================================
+export const getUser=async(req,res)=>{
+  try {
+    const id=req.params.id;
+    !id && res.status(400).json("No User Id !...");
 
-// }
+    const findUser = await User.find({_id:id});
+    !findUser && res.status(400).json("Not Found!...");
+    res.status(200).json({msg:"User Found Successful",findUser});
+  } catch (error) {
+    res.status(500).json(err);
+  }
+}
+
+
+// delete user========================
+export const deleteUser = async (req, res, next) => {
+  try {
+   const delUser= await User.findByIdAndDelete({_id:req.params.id},{new:true});
+    res.status(200).json({msg:'User has been deleted...',delUser});
+  } catch (error) {
+    next(error);
+  }
+
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import {
   test,
-//   updateUser,
-//   deleteUser,
-  createUser
+  loginUser,
+  updateUser,
+  getAllUsers,
+  getUser,
+  deleteUser,
+  createUser,
+  forgotPassword
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../middleware/verifyUser.js';
 const router = express.Router();
 
+//users routes starts here============================
 router.get('/', test);
-router.post('/register',createUser);
-// router.post('/update/:id', verifyToken, updateUser);
-// router.delete('/delete/:id', verifyToken, deleteUser);
+router.get('/userlist',verifyToken,getAllUsers);
+router.get('/:id',verifyToken,getUser);
+router.post('/login',loginUser);
+router.post('/register',verifyToken,createUser);
+router.patch('/update/:id',verifyToken, updateUser);
+router.patch('/forgot-password/:id',forgotPassword);
+router.delete('/delete/:id', verifyToken, deleteUser);
+//users routes ends here============================
+
 
 export default router;
